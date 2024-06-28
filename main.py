eacf452cad355eaaa52e40971858ae0acc26bfa4
William Gregorio <mail@williamgregorio.com>
2024-06-27 22:04:16 -0400

    imports from down this

---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import scrolledtext
+from wordcloud import WordCloud
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+import re
+
