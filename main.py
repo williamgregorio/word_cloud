3d460190e4c88c75cbddb8f29f878e731ea1ecb9
William Gregorio <mail@williamgregorio.com>
2024-06-27 22:05:43 -0400

    wc from gen wordcloud functions

--- main.py
+++ main.py
@@ -6,3 +6,26 @@ import matplotlib.pyplot as plt
 from collections import Counter
 import re
 
+def generate_wordclouds():
+    resume_text = resume_textbox.get("1.0", tk.END)
+    job_listing_text = job_listing_textbox.get("1.0", tk.END)
+
+    resume_wc = WordCloud(width=300, height=300, background_color='white').generate(resume_text)
+    job_listing_wc = WordCloud(width=300, height=300, background_color='white').generate(job_listing_text)
+
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(resume_wc, interpolation='bilinear')
+    plt.axis('off')
+    plt.title("Resume Word Cloud")
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(job_listing_wc, interpolation='bilinear')
+    plt.axis('off')
+    plt.title("Job Listing Word Cloud")
+
+    plt.show()
+
+
+
